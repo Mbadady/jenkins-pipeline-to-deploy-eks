a0098c612ab7a522889160ced1fc69f74379f32f
Mbadady <victorsomtochukwu@gmail.com>
2023-03-18 11:25:36 +0100

    Added grafana manifest file, updated Jenkinsfile

--- terraform-to-deploy-eks/provider.tf
+++ terraform-to-deploy-eks/provider.tf
@@ -1,23 +1,28 @@
-terraform {
-  required_providers {
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.0.0"
-    }
-  }
-required_providers {
-    helm = {
-      source = "hashicorp/helm"
-      version = "2.9.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     kubernetes = {
+#       source  = "hashicorp/kubernetes"
+#       version = ">= 1.19.0"
+#     }
+#     helm = {
+#       source = "hashicorp/helm"
+#       version = ">=2.9.0"
+#     }
+#   }
+# }
 
-provider "helm" {
-  kubernetes {
-    config_path = var.kube_config
-  }
-}
-provider "kubernetes" {
-  config_path = pathexpand(var.kube_config)
-}
+# provider "aws" {
+#   shared_config_files      = ["~/.aws/config"]
+#   shared_credentials_files = ["~/.aws/credentials"]
+#   profile                  = "default"
+#   region = "us-east-1"
+# }
+
+# provider "helm" {
+#   kubernetes {
+#     config_path = var.kube_config
+#   }
+# }
+# provider "kubernetes" {
+#   config_path = var.kube_config
+# }
--- terraform-to-deploy-eks/variables.tf
+++ terraform-to-deploy-eks/variables.tf
@@ -9,16 +9,16 @@ variable "private_subnets" {
   default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
 
-variable "kube_config" {
-  type    = string
-  default = "~/.kube/config"
-}
+# variable "kube_config" {
+#   type    = string
+#   default = "/home/ec2-user/.kube/config"
+# }
 
-variable "namespace" {
-  type    = string
-  default = "monitoring"
-}
-variable "kube-version" {
-  type = string
-  default = "0.3.0"
-}
+# variable "namespace" {
+#   type    = string
+#   default = "monitoring"
+# }
+# variable "kube-version" {
+#   type = string
+#   default = "0.3.0"
+# }
